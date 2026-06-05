2ddd3fe6f6a83741f56d419947bd42b96d04cc3e
pennjay237 <pennjude31@gmail.com>
2026-06-05 15:18:28 +0100

    adding backend urls

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     host: true
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'simple-peer']
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom']
   }
 })
